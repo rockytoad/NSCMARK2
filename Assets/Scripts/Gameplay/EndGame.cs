f067fc77b8897dee8f097b5ff7c95667a7734912
asdiop153246 <magicsiam001@gmail.com>
2022-12-15 17:59:17 +0700

    Add endgame

--- Assets/Scripts/Gameplay/EndGame.cs
+++ Assets/Scripts/Gameplay/EndGame.cs
@@ -1,18 +1,32 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.SceneManagement;
 public class EndGame : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public GameObject EndSafe;
+    PlayerMovementTutorial PlayerController;
+    private void OnTriggerEnter(Collider other)
     {
-        
+        if (other.gameObject.tag == "Finish")
+        {
+            Debug.Log("Hit End");
+            // Get the PlayerController component from the player
+            PlayerController = GameObject.Find("Player").GetComponent<PlayerMovementTutorial>();
+            if (PlayerController.isSafe == true)
+            {
+                Debug.Log("Hit End");
+                StartCoroutine(delay());
+            }
+
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    IEnumerator delay()
     {
-        
+        EndSafe.gameObject.SetActive(true);
+        yield return new WaitForSeconds(5.0f);
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        EndSafe.gameObject.SetActive(false);
     }
 }
