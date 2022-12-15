6c5ca16a551357f6803bd203aa3f6819746cf90e
asdiop153246 <magicsiam001@gmail.com>
2022-12-15 19:46:23 +0700

    Add cooldown drop

--- Assets/Scripts/InventorySimple/DropSimple.cs
+++ Assets/Scripts/InventorySimple/DropSimple.cs
@@ -10,23 +10,32 @@ public class DropSimple : MonoBehaviour
 
     [Header("Player")]
     public GameObject Orientation;
-    
+
+    [SerializeField] bool isCooldown = false;
         PlayerMovementTutorial PlayerController;
     
     void Update()
     {
         PlayerController = GameObject.Find("Player").GetComponent<PlayerMovementTutorial>();
-        if (Input.GetKeyDown(KeyCode.Q) && PlayerController.Key == true)
+        if (Input.GetKeyDown(KeyCode.Q) && PlayerController.Key == true && isCooldown == false)
         {
             Instantiate(Key, Orientation.transform.position + (Orientation.transform.forward * 0.5f), Orientation.transform.rotation);
             Debug.Log("Drop Key");
             PlayerController.Key = false;
+            StartCoroutine(Delay());
         }
-        else if (Input.GetKeyDown(KeyCode.Q) && PlayerController.Hammer == true)
+        else if (Input.GetKeyDown(KeyCode.Q) && PlayerController.Hammer == true && isCooldown == false)
         {
             Instantiate(Hammer, Orientation.transform.position + (Orientation.transform.forward * 0.5f), Orientation.transform.rotation);
             Debug.Log("Drop Hammer");
             PlayerController.Hammer = false;
         }
+    }
+
+    IEnumerator Delay()
+    {
+        isCooldown = true;
+        yield return new WaitForSeconds(3f);
+        isCooldown = false;
     }
 }
